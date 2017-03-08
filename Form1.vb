3ad014388d89817c033c29f1bfdac8fbe1a98dba
TD <davi1td@hotmail.com>
2017-03-08 10:28:23 -0500

    another test line

--- Form1.vb
+++ Form1.vb
@@ -44,5 +44,6 @@ Public Class Form1
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         'ghgjhgjhg
+        'dfgdggfdgfdgdfgfdgfdgfdg
     End Sub
 End Class
