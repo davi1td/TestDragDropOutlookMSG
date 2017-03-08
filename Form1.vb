8aec88b1cc049b1c84c1a32e92566cff720f3021
TD <davi1td@hotmail.com>
2017-03-08 10:13:00 -0500

    Add project files.

---
+++ Form1.vb
@@ -0,0 +1,48 @@
+﻿Imports Outlook = NetOffice.OutlookApi
+
+Public Class Form1
+    Private Sub TextBox1_DragEnter(sender As Object, e As DragEventArgs)
+        e.Effect = DragDropEffects.All
+    End Sub
+
+    Private Sub Button1_Click_1(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim myString As String = TextBox2.Text
+        Dim index As Integer = ListBox1.FindString(myString, 1)
+        If index <> -1 Then
+            ' Select the found item:
+            ListBox1.SetSelected(index, True)
+            MessageBox.Show(ListBox1.Items(index + 2) & "  : count : " & ListBox1.Items.Count.ToString)
+        Else
+            MessageBox.Show("Item not found.")
+        End If
+    End Sub
+
+    Private Sub ListBox1_DragEnter(sender As Object, e As DragEventArgs) Handles ListBox1.DragEnter
+        e.Effect = DragDropEffects.All
+    End Sub
+
+    Private Sub ListBox1_DragDrop(sender As Object, e As DragEventArgs) Handles ListBox1.DragDrop
+        Dim app As Outlook.Application = New Outlook.Application()
+        Dim appexplorer As Outlook.Explorer = app.ActiveExplorer
+        If e.Data.GetDataPresent("FileGroupDescriptor") Then
+
+            For Each testForEmail In appexplorer.Application.ActiveExplorer.Selection()
+                If TypeOf testForEmail Is Outlook.MailItem Then
+                    ProcessEmail(testForEmail)
+                End If
+            Next
+        End If
+
+    End Sub
+    Private Sub ProcessEmail(theEmail As Outlook.MailItem)
+        ListBox1.Items.Clear()
+        ListBox1.Items.AddRange(theEmail.Body.Split(vbLf)) ' need to know the data, look at a line
+        'vblf was at the beginning of each line using newline or vbcr or whatever, which affected searches
+    End Sub
+
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("TestDragDropOutlookMSG")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("TestDragDropOutlookMSG")>
+<Assembly: AssemblyCopyright("Copyright ©  2017")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("2299f746-ec03-45e6-888a-1f76d89bf1b8")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
